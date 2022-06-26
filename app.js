e3e05097e9988aef3ffaf7cd9b6bfe10dadce5d5
Githmin Jayawardhana <githmin21@gmail.com>
2022-06-26 13:05:49 +0530

    added basic funtions

---
+++ app.js
@@ -0,0 +1,21 @@
+let sum = 0;
+
+function add (firstInput , lastInput){ 
+    sum = firstInput + lastInput;
+    return sum;
+}
+
+function substract (firstInput, lastInput){
+    sum = firstInput - lastInput;
+    return sum;
+}
+
+function multiply (firstInput, lastInput){
+    sum = firstInput * lastInput;
+    return sum;
+}
+
+function divide (firstInput, lastInput) {
+    sum = firstInput / lastInput;
+    return sum;
+}
