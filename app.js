34debe1e854e4e20e7e7c1732102c01a0ae0dd30
Githmin Jayawardhana <githmin21@gmail.com>
2022-06-26 19:26:41 +0530

    minor color changes and fixes

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-let sum = 0;
+// let sum = 0;
 
-function add (firstInput , lastInput){ 
-    sum = firstInput + lastInput;
-    return sum;
+var reply_click = function()
+{
+    alert(this.id);
 }
+document.getElementById('seven').onclick = reply_click;
+// document.getElementById('2').onclick = reply_click;
+// document.getElementById('3').onclick = reply_click;
 
-function substract (firstInput, lastInput){
-    sum = firstInput - lastInput;
-    return sum;
-}
 
-function multiply (firstInput, lastInput){
-    sum = firstInput * lastInput;
-    return sum;
-}
 
-function divide (firstInput, lastInput) {
-    sum = firstInput / lastInput;
-    return sum;
-}
+
+
+// function add (firstInput , lastInput){ 
+//     sum = firstInput + lastInput;
+//     return sum;
+// }
+
+// function substract (firstInput, lastInput){
+//     sum = firstInput - lastInput;
+//     return sum;
+// }
+
+// function multiply (firstInput, lastInput){
+//     sum = firstInput * lastInput;
+//     return sum;
+// }
+
+// function divide (firstInput, lastInput) {
+//     sum = firstInput / lastInput;
+//     return sum;
+// }
