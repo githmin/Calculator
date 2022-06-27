63023c41234073106eaf6f851e05d3bb96b240e0
Githmin Jayawardhana <githmin21@gmail.com>
2022-06-27 11:42:49 +0530

    added calc functionality

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
-// let sum = 0;
+let display = document.getElementById('display-text');
 
+let buttons = Array.from(document.getElementsByClassName('btns'));
 
-var reply_click = function()
-{
-    alert(this.id);
-    
-}
-document.getElementById('seven').onclick = reply_click;
-// document.getElementById('2').onclick = reply_click;
-// document.getElementById('3').onclick = reply_click;
-
-
-
-
-
-// function add (firstInput , lastInput){ 
-//     sum = firstInput + lastInput;
-//     return sum;
-// }
-
-// function substract (firstInput, lastInput){
-//     sum = firstInput - lastInput;
-//     return sum;
-// }
-
-// function multiply (firstInput, lastInput){
-//     sum = firstInput * lastInput;
-//     return sum;
-// }
-
-// function divide (firstInput, lastInput) {
-//     sum = firstInput / lastInput;
-//     return sum;
-// }
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case 'AC':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
